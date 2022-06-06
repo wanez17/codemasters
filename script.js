7d3447f600b4b2aea52c64c805a3aa1d6c6d77db
Mohammed Awal <77703552+wanez17@users.noreply.github.com>
2022-06-05 18:58:45 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+const duration = document.getElementById("duration")
+const cert = document.getElementById("cert")
+let Switch  = document.getElementById("switch")
+
+
+
+function insertData(){
+	document.getElementById("switch").innerHTML = "This course is for anyone who is passionate about learning web development and there is no requirement";
+}
+
+function insertData1(){
+	document.getElementById("switch").innerHTML = "6 months";
+}
+
+function insertData2(){
+	document.getElementById("switch").innerHTML = "There will be certification depending on the organisation that you study with, certificates will differ.";
+}
+
+
+
+
